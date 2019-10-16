e581441f364fe28a3acbe339f67217258ac2da93
Tam <minhtamtt@gmail.com>
2019-10-16 11:05:19 +0700

    push source

--- webpack.js
+++ webpack.js
@@ -1,10 +1,9 @@
-exports.assets = {
-    "assets":{
-        "copyDirectory":[
-            ["public","angular"],
-        ],
-        "copyInternal":[
-            ["public/index.html","resources/views/index.blade.php"]
-        ]
-    }
+
+exports.combine = function(mix){
+    let source = `${__dirname}/src`;
+    let dest = 'public';
+    mix.sass(`${source}/assets/sass/asset.scss`, `${dest}/css/assets/asset.css`).version();
+    mix.js([
+        `${source}/assets/js/asset.js`
+    ], `${dest}/js/asset/asset.js`).version();
 };
