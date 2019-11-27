962917c5d7320d6fbed833c93ddfdd30c8fc3fcd
Tam <minhtamtt@gmail.com>
2019-11-27 21:50:50 +0700

    add permissions

--- src/Listeners/UpdatePermissionForOrganizationOwner.php
+++ src/Listeners/UpdatePermissionForOrganizationOwner.php
@@ -3,11 +3,19 @@
 namespace Shura\Asset\Listeners;
 
 use Shura\Asset\Helpers\Helper;
+use Core\Organization\Listeners\Trails\UpdateDefaultGroupPermissions;
 
-class UpdatePermissionForOrganizationOwner extends \Core\Organization\Listeners\UpdatePermissionForOrganizationOwner
+class UpdatePermissionForOrganizationOwner
 {
     public $moduleName = 'shura\asset';
+    use UpdateDefaultGroupPermissions;
     public function getPermissions(){
         return Helper::collect('permissions.json');
+    }
+    public function handle($event){
+        $module = $event->module;
+        if($event->organization && $module->module_name === $this->moduleName){
+            $this->updateOrganizationOwnerPermissions($event->organization);
+        }
     }
 }
